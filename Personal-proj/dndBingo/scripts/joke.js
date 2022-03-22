d9bd34e9b8440508ec5a02a7af25213b5a7ce612
Ryan Ehmann <RyanKEhmann@gmail.com>
2022-03-22 16:30:25 -0400

    added dndbingo

---
+++ Personal-proj/dndBingo/scripts/joke.js
@@ -0,0 +1,25 @@
+function badBackground() {
+
+    if (document.querySelector('#bingo')) {
+        var num = Math.floor(Math.random() * 20) + 1
+        //num = 1;
+        if (num== 1) {
+            document.querySelector('#bingo').classList.toggle("green-background")
+            document.querySelector('#bingo').classList.toggle("bad-background")
+            document.querySelector('#bingo').innerHTML = "You Rolled a Natural 1, Refresh the page to remove this message"
+        }
+
+    }
+    console.log(num)
+}
+
+function forcedBackground(newbackground,oldbackground) {
+    document.querySelector('#card__wrapper').classList.toggle(oldbackground)
+    document.querySelector('#card__wrapper').classList.toggle(newbackground)
+    
+    
+}
+
+
+
+//--------------------------------------
